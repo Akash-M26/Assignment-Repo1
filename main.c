ca808ec0dfa1d02f5475909560dbaf5725b8bc42
AKA <aka@gmail.com>
2024-10-22 10:26:55 +0530

    Adding 2 files in git hub

---
+++ main.c
@@ -0,0 +1,9 @@
+#include <stdio.h>
+
+// Function declarations (prototypes)
+void factorial();
+
+int main() {
+    factorial();  // Call to factorial function
+    return 0;     // Return 0 to indicate successful execution
+}
